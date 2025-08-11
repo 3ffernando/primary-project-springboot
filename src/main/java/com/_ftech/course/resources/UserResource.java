814e36fa29a0feb8b1a4a793278cfc6534fa78fb
3ffernando <srfernandoff@hotmail.com>
2025-08-11 20:45:54 -0300

    User entity and resource

---
+++ src/main/java/com/_ftech/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com._ftech.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com._ftech.course.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+		@GetMapping
+		public ResponseEntity<User> findAll(){
+			User u  = new User(1L,"Maria","maria@gmail.com","6599999999","123456");
+			return ResponseEntity.ok().body(u);
+	}
+}
